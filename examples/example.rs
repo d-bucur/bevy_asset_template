d23d139aa716a294cb1e0edf42d06987aadb02a8
Dorian Bucur <sirdorius@gmail.com>
2023-08-27 17:42:09 +0200

    ran cargo fmt

--- examples/example.rs
+++ examples/example.rs
@@ -3,10 +3,5 @@ use your_asset_name::MyPlugin;
 
 // An example of how to use the plugin in a Bevy app
 fn main() {
-    App::new()
-        .add_plugins((
-            DefaultPlugins,
-            MyPlugin,
-        ))
-        .run();
+    App::new().add_plugins((DefaultPlugins, MyPlugin)).run();
 }
